1384046197ab57bc648a4b59823a19454c2c5e65
Prakhar Saxena <prakharsaxena2806@gmail.com>
2023-09-09 00:04:58 +0530

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,41 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb-typescript',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    'prettier',
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 0,
+  },
+};
---
+++ .prettierrc.cjs
@@ -0,0 +1,6 @@
+module.exports = {
+  "trailingComma": "es2021",
+  "tabWidth": 2,
+  "semi": true,
+  "singleQuote": true
+}
